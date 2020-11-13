19a2a13ffdb36dc48862c81bc867846225af5b9f
Sarah Carlson <sbcarls@gmail.com>
2020-11-13 10:41:15 -0500

    modifed questions, started template set up

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ inquire
     },
     {
         type: "input",
-        message: "Table of contents?",
+        message: "What is the table of contents?",
         name: "contents",
         validate: (value) =>{
             if(value){return true}
@@ -41,7 +41,7 @@ inquire
     },
     {
         type: "input",
-        message: "",
+        message: "What is the app used for?",
         name: "usage",
         validate: (value) =>{
             if(value){return true}
@@ -49,24 +49,58 @@ inquire
         },
     },
     {
+        type: "input",
+        message: "How do you use the app?",
+        name: "usage",
+        validate: (value) =>{
+            if(value){return true}
+            else{return "give me a value"}
+        },
+    },
+    {    //need license choices here //
         type: "checkbox",
-        message: "",
+        message: "Does this app require any licenses?",
         name: "license",
         choices: ["","","","",]
     },
     {
         type: "input",
-        message: "",
+        message: "What are the contributing instructions for this project?",
         name: "contributing"
     },
     {
         type: "input",
-        message: "",
+        message: "How would people test this project?",
         name: "tests"
     },
     {
         type: "input",
-        message: "Any questions, contact me sbcarls@gmail.com",
+        message: "Any questions, contact me at sbcarls@gmail.com",
         name: "questions",
     },
-])
+]) .then(({
+    title,
+    description, 
+    contents,
+    installation,
+    usage,
+    usage2, 
+    license,
+    contributing,
+    tests,
+    questions,
+}) => {
+    let template = ` # ${title}
+    * [description] (#description)
+    * [contents] (#contents)
+    * [installation] (#installation)
+    * [usage] (#usage)
+    * [usage2] (#usage2)
+    * [license] (#license)
+    * [contributing] (#contributing)
+    * [tests] (#tests)
+    * [questions] (#questions)
+
+
+    `
+})
